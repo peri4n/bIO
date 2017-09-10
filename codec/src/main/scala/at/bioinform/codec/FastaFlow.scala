7f29d922f59aa2ce5e3a89e7114c7169c460549b
Fabian Bull <fabian.bull@bioinform.at>
2017-09-10 16:07:14 +0200

    bIO/10 - Rftd FastaFlow to process line-by-line
    - It turns out to be much more maintainable when passing input
      line-by-line.

--- codec/src/main/scala/at/bioinform/codec/FastaFlow.scala
+++ codec/src/main/scala/at/bioinform/codec/FastaFlow.scala
@@ -4,11 +4,10 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 
 import akka.stream._
-import akka.stream.scaladsl.{FileIO, Source}
+import akka.stream.scaladsl.{FileIO, Framing, Source}
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
 import akka.util.ByteString
 
-import scala.collection.mutable
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
@@ -17,72 +16,58 @@ import scala.util.{Failure, Success, Try}
  */
 object FastaFlow extends GraphStage[FlowShape[ByteString, FastaEntry]] {
 
+  val MAX_LINE_SIZE = 200
+
   /**
    * Utility method to easily create a processor from a given path.
    *
    * @param path path to a FASTA formatted file.
-   * @return a flow porviding [[FastaEntry]]s
+   * @return a flow providing [[FastaEntry]]s
    */
   def from(path: Path): Source[FastaEntry, Future[IOResult]] = {
-    FileIO.fromPath(path).via(FastaFlow)
+    FileIO.fromPath(path)
+      .via(Framing.delimiter(ByteString(System.lineSeparator()), MAX_LINE_SIZE))
+      .via(FastaFlow)
+      .named("FastaFlow")
   }
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
 
-    private var chunkAccumulator = StringBuilder.newBuilder
+    private var sequenceBuilder = new StringBuilder(1000)
 
-    private val processedEntries = new mutable.Queue[FastaEntry]()
+    private var currentHeader: Option[FastaHeader] = None
 
-    def parseHeader(chunk: String): Try[(FastaHeader, String)] =
+    def parseHeader(chunk: String): Try[FastaHeader] =
       if (!chunk.startsWith(">")) {
         Failure(new RuntimeException(s"Expected a '>' at the start of a fasta entry but read: '${chunk.head}'"))
       } else {
-        val (headerLine, rest) = chunk.tail.span(_ != '\n')
-        val (id, desc) = headerLine.span(_ != ' ')
-        val maybeDescription = if (desc.isEmpty) None else Some(desc.trim)
-        Success(FastaHeader(id, maybeDescription), rest)
+        val (id, description) = chunk.tail.span(_ != ' ')
+        val maybeDescription = if (description.isEmpty) None else Some(description.trim())
+        Success(FastaHeader(id, maybeDescription))
       }
 
-    private def parseSequence(rest: String): Try[String] = Try(rest.filter(_ != '\n'))
-
-    private def extractEntry(chunk: String): Try[FastaEntry] =
-      for {
-        (header, rest) <- parseHeader(chunk)
-        sequence <- parseSequence(rest)
-      } yield FastaEntry(header, sequence)
-
-    private def chopOfEntry(builder: StringBuilder): (String, mutable.StringBuilder) = {
-      val startOfNextEntry = builder.tail.indexWhere(_ == '>') + 1
-      val (seq, rest) = builder.splitAt(startOfNextEntry)
-      (seq.result(), rest)
-    }
-
-    private def containsCompleteEntries(accum: mutable.StringBuilder): Boolean = accum.count(_ == '>') > 1
+    private def isHeaderLine(line: String): Boolean = line.startsWith(">")
 
     setHandler(in, new InHandler {
+
       override def onPush(): Unit = {
-        chunkAccumulator ++= grab(in).decodeString(StandardCharsets.UTF_8)
-        if (containsCompleteEntries(chunkAccumulator)) {
-          while (containsCompleteEntries(chunkAccumulator)) {
-            val (seq, rest) = chopOfEntry(chunkAccumulator)
-            chunkAccumulator = rest
-            val entry = extractEntry(seq)
-            if (entry.isSuccess) {
-              processedEntries.enqueue(entry.get)
-            } else {
-              // fail silently: muhahahahahah evil laugh
-            }
-          }
-          push(out, processedEntries.dequeue)
-        } else {
+        val line = grab(in).decodeString(StandardCharsets.UTF_8).trim
+        if (isHeaderLine(line) && currentHeader.isEmpty) { // first header line
+          currentHeader = Some(parseHeader(line).get)
+          pull(in)
+        } else if (isHeaderLine(line) && currentHeader.isDefined) {
+          push(out, FastaEntry(currentHeader.get, sequenceBuilder.result()))
+          sequenceBuilder.clear()
+          currentHeader = Some(parseHeader(line).get)
+        } else { // a sequence file
+          sequenceBuilder ++= line
           pull(in)
         }
       }
 
       override def onUpstreamFinish(): Unit = {
-        if(!chunkAccumulator.isEmpty) {
-          val (_, seq) = chopOfEntry(chunkAccumulator)
-          push(out, extractEntry(seq.result()).get)
+        if (sequenceBuilder.nonEmpty) {
+          push(out, FastaEntry(currentHeader.get, sequenceBuilder.result()))
         }
         super.onUpstreamFinish()
       }
@@ -90,11 +75,7 @@ object FastaFlow extends GraphStage[FlowShape[ByteString, FastaEntry]] {
 
     setHandler(out, new OutHandler {
       override def onPull(): Unit =
-        if (processedEntries.isEmpty) {
-          pull(in)
-        } else {
-          push(out, processedEntries.dequeue)
-        }
+        pull(in)
     })
 
   }
