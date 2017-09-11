8ce040d5e6eb53f5a63f95f184b22c53495097b0
Fabian Bull <fabian.bull@bioinform.at>
2017-09-11 19:33:54 +0200

    Create a File Upload component (#21)
    bio/7 - Add a FileUpload component
    
    - The main goal of this issue was to investigate how we can overcome the
      need to save a uploaded file on the server disk. This has been
      achieved with a custom BodyParser that parses FASTA files that are
      uploaded raw.
    - To use the FastaFlow, `webapp` now depends on `codec`.
    - The FileUpload component speaks with the backend and logs its
      response.
    - I had to remove the security filters to make this work. This is a
      temporary workaround.
    - The File Upload Controller returns Json

--- webapp/app/controllers/HomeController.scala
+++ webapp/app/controllers/HomeController.scala
@@ -2,12 +2,43 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.mvc.{BaseController, ControllerComponents}
+import akka.stream.scaladsl.{Flow, Framing, Keep, Sink}
+import akka.util.ByteString
+import at.bioinform.codec.{FastaEntry, FastaFlow}
+import play.api.Logger
+import play.api.libs.json.{Json, Writes}
+import play.api.libs.streams.Accumulator
+import play.api.mvc.{BaseController, BodyParser, ControllerComponents}
 
-class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  val logger: Logger = Logger(this.getClass())
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  val bp: BodyParser[List[FastaEntry]] = BodyParser { req =>
+    logger.info("Incoming requst: " + req)
+    val sink = Flow[ByteString]
+      .via(Framing.delimiter(ByteString(System.lineSeparator()), 200))
+      .via(FastaFlow)
+      .toMat(Sink.fold(List.empty[FastaEntry])(_ :+ _))(Keep.right)
+    Accumulator(sink).map(Right.apply)
+  }
+
+  implicit val toJson = new Writes[FastaEntry] {
+    override def writes(o: FastaEntry) = Json.obj(
+      "id" -> o.header.id,
+      "description" -> o.header.description,
+      "sequence" -> o.sequence
+    )
+  }
+
+  def upload = Action(bp) { request =>
+    Ok(Json.toJson(request.body))
   }
 
 }
