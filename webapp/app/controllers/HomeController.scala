44e7a8818d711f94b0460918571bd5f6e38cf24b
Fabian Bull <fabian.bull@bioinform.at>
2017-09-11 06:16:07 +0200

    bio/7 - Add a FileUpload component
    - The FileUpload component speaks with the backend and logs its
      response.
    - I had to remove the security filters to make this work. This is a
      temporary workaround.

--- webapp/app/controllers/HomeController.scala
+++ webapp/app/controllers/HomeController.scala
@@ -2,9 +2,10 @@ package controllers
 
 import javax.inject.Inject
 
-import akka.stream.scaladsl.{Flow, Keep, Sink}
+import akka.stream.scaladsl.{Flow, Framing, Keep, Sink}
 import akka.util.ByteString
 import at.bioinform.codec.{FastaEntry, FastaFlow}
+import play.api.Logger
 import play.api.libs.streams.Accumulator
 import play.api.mvc.{BaseController, BodyParser, ControllerComponents}
 
@@ -12,12 +13,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
+  val logger: Logger = Logger(this.getClass())
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   val bp: BodyParser[List[FastaEntry]] = BodyParser { req =>
+    logger.info("Incoming requst: " + req)
     val sink = Flow[ByteString]
+      .via(Framing.delimiter(ByteString(System.lineSeparator()), 200))
       .via(FastaFlow)
       .toMat(Sink.fold(List.empty[FastaEntry])(_ :+ _))(Keep.right)
     Accumulator(sink).map(Right.apply)
--- webapp/src/app.jsx
+++ webapp/src/app.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render((
-    <div>
-        <form>
-            <label for="search-bar">
-              Search bar:
-            </label>
-            <input type="text" name="search-term" id="search-bar"/>
-        </form>
-    </div>), document.getElementById("app"));
+class FileUpload extends React.Component {
+    render() {
+        return (
+            <input type="file" onChange={event => this.logFile(event)}/>
+        );
+    }
+
+    logFile(event) {
+        const input = event.target;
+        const reader = new FileReader();
+
+        reader.onload = function () {
+            console.log(reader.result)
+            const oReq = new XMLHttpRequest();
+            oReq.open("POST", "http://localhost:9000/", true);
+            oReq.onload = () => {
+                console.log(oReq.response);
+            };
+            oReq.send(reader.result);
+        };
+        reader.readAsBinaryString(input.files[0]);
+    }
+}
+
+render(<FileUpload />, document.getElementById("app"));
